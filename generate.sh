6b574f255b8a1a248187c8fcce7c3f0790ab4ada
Dafydd Vaughan <dafydd.vaughan@cedyrn.com>
2022-12-21 20:50:10 +0000

    add php 8.1

--- generate.sh
+++ generate.sh
@@ -62,3 +62,6 @@ gen 7.4 7.4-cli cli nocron
 gen 8.0 8.0-apache apache nocron composer
 gen 8.0 8.0-apache-cron apache cron composer
 gen 8.0 8.0-cli cli nocron composer
+gen 8.1 8.1-apache apache nocron composer
+gen 8.1 8.1-apache-cron apache cron composer
+gen 8.1 8.1-cli cli nocron composer
