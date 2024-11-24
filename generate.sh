68ccbe1f111871b29c906f7f2eea1722174ef62c
Dafydd Vaughan <dafydd.vaughan@cedyrn.com>
2024-11-24 12:00:21 +0000

    Fix mistake that is breaking php extensions

--- generate.sh
+++ generate.sh
@@ -24,7 +24,7 @@ gen() {
   echo '   zip \' >> ${NAME}/Dockerfile
   echo '   unzip \' >> ${NAME}/Dockerfile
   echo '   && rm -rf /var/lib/apt/lists/* \' >> ${NAME}/Dockerfile
-  echo '   docker-php-ext-install pdo pdo_mysql mysqli intl' >> ${NAME}/Dockerfile
+  echo '   && docker-php-ext-install pdo pdo_mysql mysqli intl' >> ${NAME}/Dockerfile
   echo '' >> ${NAME}/Dockerfile
 
   echo '# Harden PHP' >> ${NAME}/Dockerfile
